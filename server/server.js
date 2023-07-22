2c13267501310b9ba5bafc9a5120955caf2f0dd7
mrahmanashiq <mizanur.r.ashiq@gmail.com>
2023-07-22 08:15:34 +0600

    add adminseed

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 import { app } from './src/config/lib/app.js';
 import { config } from './src/config/lib/config.js';
+import { seedAdmin } from './src/modules/platform-users/admin.seed.js';
 
 mongoose.set('strictQuery', false);
 
@@ -15,7 +16,8 @@ const startServer = async () => {
     console.log('MongoDB is connected');
 
     app.listen(config.port);
-
+    await seedAdmin();
+    
     console.log(`Server is running on port ${config.port}`);
   } catch (error) {
     console.log(error);
