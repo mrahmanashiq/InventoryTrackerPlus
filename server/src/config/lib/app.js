83478930597de3eddbcd05ce1675a30017ac68de
mrahmanashiq <mizanur.r.ashiq@gmail.com>
2023-07-22 08:13:54 +0600

    change config

--- server/src/config/lib/app.js
+++ server/src/config/lib/app.js
@@ -1,23 +1,27 @@
-import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import express from 'express';
 import morgan from 'morgan';
 
+import { rootRouter } from '../../modules/core/root.routes.js';
 import { config } from './config.js';
 
 const app = express();
 
 app.use(
-  cors({
-    origin: [config.clientOrigin],
-    credentials: true,
-  })
+	cors({
+		origin: [config.clientOrigin],
+		credentials: true,
+	})
 );
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
+
+app.use(rootRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 export { app };
-
